cdeb70bd8451b7204031b145f79e5de5a37ad27d
Jacob Ide <jacobmeide@gmail.com>
2021-03-05 14:05:14 -0600

    more styling

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             url: '/api/beers',
             success: function(data) {
                 $.each(data, function(i, item){
-                    $('#beersloader').append('<div class=" mt-3 mr-5"><div class="mt-3 card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">'+ item.name +'</h5><h6>' + item.brewer_name + '</h6><p>' + item.style + '</p><p class="card-text">IBU: ' + item.ibu + '</p><p class="card-text">ABV: ' + item.abv + '</p><a href="#" class="btn btn-warning">More</a></div></div></div>');
+                    $('#beersloader').append('<div class=" mt-3 mr-5"><div class="mt-3 beercard card" style="width: 18rem;"><div class="card-body"><h3 class="card-title">'+ item.name +'</h3><h6>' + item.brewer_name + '</h6><hr><p>' + item.style + '</p><div class="card-inset"><p class="card-text mt-1">IBU: ' + item.ibu + '</p><p class="card-text mt-1">ABV: ' + item.abv + '</p></div><a href="#" class="btn more-button">More</a></div></div></div>');
                 });
             }
         });
@@ -16,7 +16,7 @@ $(document).ready(function () {
             url: '/api/brewers',
             success: function(data) {
                 $.each(data, function(i, item){
-                    $('#brewersloader').append('<div class="card mt-3 mr-3" style="width: 18rem;"><div class="card-body"><h5 class="card-title">'+ item.name +'</h5><p> Founded: ' + item.founded + '</p><p>' + item.address + '</p><p>' + item.email + '</p><p class="card-text">' + item.phone + '</p><p class="card-text"><a href="' + item.website + '">'+ item.website + '</a></p><a href="#" class="btn btn-warning">More</a></div></div>');
+                    $('#brewersloader').append('<div class="card brewercard mt-3 mr-3" style="width: 18rem;"><div class="card-body"><h5 class="card-title">'+ item.name +'</h5><p> Founded: ' + item.founded + '</p><hr><p>' + item.address + '</p><p>' + item.email + '</p><p class="card-text">' + item.phone + '</p><p class="card-text"><a href="' + item.website + '">'+ item.website + '</a></p><a href="#" class="btn btn-warning">More</a></div></div>');
                 });
             }
         });
@@ -36,11 +36,11 @@ $(document).ready(function () {
                     set2.push(data[i]);
                 }
                 $.each(set1, function(i, item){
-                    $('.carousel-inner').append('<div class="carousel-item active"><div class="card"><div class="card-body"><h4 class="card-title">' + item.name + '</h4><h5 class="card-title">' + item.brewer_name +'</h5><h6 class="text-muted card-subtitle mb-2">' + item.style + '</h6><p class="card-text">' + item.description + '</p></div></div></div>');
+                    $('.carousel-inner').append('<div class="carousel-item active"><div class="card"><div class="card-body"><h4 class="card-title">' + item.name + '</h4><h5 class="card-title">' + item.brewer_name +'</h5><h6 class="text-muted card-subtitle mb-2">' + item.style + '</h6><p class="carousel-text">' + item.description + '</p></div></div></div>');
                 });
                 console.log(set2);
                 $.each(set2, function(i, item){
-                    $('.carousel-inner').append('<div class="carousel-item"><div class="card"><div class="card-body"><h4 class="card-title">' + item.name + '</h4><h5 class="card-title">' + item.brewer_name +'</h5><h6 class="text-muted card-subtitle mb-2">' + item.style + '</h6><p class="card-text">' + item.description + '</p></div></div></div>');
+                    $('.carousel-inner').append('<div class="carousel-item"><div class="card"><div class="card-body"><h4 class="card-title">' + item.name + '</h4><h5 class="card-title">' + item.brewer_name +'</h5><h6 class="text-muted card-subtitle mb-2">' + item.style + '</h6><p class="carousel-text">' + item.description + '</p></div></div></div>');
                 });
             }
         });
